35a3d596cef82faebb21b275c1f95cb1e8efb019
Lilian Yang <mlilianyang@gmail.com>
2020-01-15 11:11:25 -0500

    initial commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Tech on 2020-01-15.
+//  Copyright © 2020 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
