70455b49b855a52ca1c74c91353843360c17cf34
Lilian <mlilianyang@gmail.com>
2020-01-22 11:41:46 -0500

    added code in controller

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,7 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberOnScreen: Double = 0;
+    var previousNumber: Double = 0;
+    var performingMath = false;
+    var operation = 0;
+    
+    @IBOutlet weak var label: UITextField!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        
+        if performingMath == true {
+            label.text = String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
+        }
+        else{
+            label.text = label.text! + String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+        }
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+    
+        if label.text != "" && sender.tag != 11 && sender.tag != 16{
+            previousNumber = Double(label.text!)!
+            
+            if sender.tag == 12 {
+                label.text = "/";
+            }
+            if sender.tag == 13{
+                label.text = "x";
+            }
+            
+        }
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
